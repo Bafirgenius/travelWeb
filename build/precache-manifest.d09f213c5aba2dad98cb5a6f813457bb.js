6ac72b344150f8dabc7bb088dc6debc9f4b7f4c9
bafirgenius <bafirgenius@gmail.com>
2021-07-13 15:57:33 +0700

    build

---
+++ build/precache-manifest.d09f213c5aba2dad98cb5a6f813457bb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a5544fa3bb7de44119644b88d7385015",
+    "url": "/index.html"
+  },
+  {
+    "revision": "728797646c293a065e66",
+    "url": "/static/css/main.56da0228.chunk.css"
+  },
+  {
+    "revision": "36128ff7f373e5beff7a",
+    "url": "/static/js/2.1442e098.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/static/js/2.1442e098.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "728797646c293a065e66",
+    "url": "/static/js/main.6162c8be.chunk.js"
+  },
+  {
+    "revision": "6fc4f2c3fd5ae8d05fd1",
+    "url": "/static/js/runtime-main.4ba1596e.js"
+  }
+]);
